622aade86865ac9dd4b95b8dd017729e4020e182
Yogesh <yogeshgajjar75@gmail.com>
2021-04-27 21:35:49 -0700

    updated the constructor

--- src/ukf.cpp
+++ src/ukf.cpp
@@ -8,24 +8,29 @@ using Eigen::VectorXd;
  * Initializes Unscented Kalman filter
  */
 UKF::UKF() {
-  // if this is false, laser measurements will be ignored (except during init)
-  use_laser_ = true;
+  
+  use_laser_ = true; // if this is false, laser measurements will be ignored (except during init)
+  use_radar_ = true; // if this is false, radar measurements will be ignored (except during init)
+  is_initialized_ = false;
 
-  // if this is false, radar measurements will be ignored (except during init)
-  use_radar_ = true;
 
-  // initial state vector
-  x_ = VectorXd(5);
-
-  // initial covariance matrix
-  P_ = MatrixXd(5, 5);
+  x_ = VectorXd(5); // initial state vector
+  P_ = MatrixXd(5, 5); // initial covariance matrix
+  P_ << 1,0,0,0,0,
+        0,1,0,0,0,
+        0,0,1,0,0,
+        0,0,0,1,0,
+        0,0,0,0,1;
+  
+  // initial predicted sigma points in state space
+  Xsig_pred_ = MatrixXd(n_x_, 2 * n_aug_ + 1);
 
   // Process noise standard deviation longitudinal acceleration in m/s^2
   std_a_ = 30;
 
   // Process noise standard deviation yaw acceleration in rad/s^2
   std_yawdd_ = 30;
-  
+
   /**
    * DO NOT MODIFY measurement noise values below.
    * These are provided by the sensor manufacturer.
@@ -45,15 +50,34 @@ UKF::UKF() {
 
   // Radar measurement noise standard deviation radius change in m/s
   std_radrd_ = 0.3;
-  
+
   /**
    * End DO NOT MODIFY section for measurement noise values 
    */
-  
+
   /**
    * TODO: Complete the initialization. See ukf.h for other member properties.
    * Hint: one or more values initialized above might be wildly off...
    */
+  
+   // set state dimension
+  n_x_ = 5;
+
+  // set augmented dimension
+  n_aug_ = 7;
+
+  // define spreading parameter
+  lambda_ = 3 - n_aug_;
+
+  // set vector for weights
+  weights_ = VectorXd(2 * n_aug_+1);
+  double weight_0 = lambda_/(lambda_+n_aug_);
+  double weight = 0.5/(lambda_+n_aug_);
+  weights_(0) = weight_0;
+
+  for(int i=1; i < 2*n_aug_+1; i++) {
+    weights_(i) = weight;
+  }
 }
 
 UKF::~UKF() {}
@@ -63,6 +87,8 @@ void UKF::ProcessMeasurement(MeasurementPackage meas_package) {
    * TODO: Complete this function! Make sure you switch between lidar and radar
    * measurements.
    */
+  
+  // define the delta_t here 
 }
 
 void UKF::Prediction(double delta_t) {
@@ -71,6 +97,11 @@ void UKF::Prediction(double delta_t) {
    * Modify the state vector, x_. Predict sigma points, the state, 
    * and the state covariance matrix.
    */
+
+  //this includes two - three functions
+  // generateSigmaPoints()
+  // augmentSigmaPoints
+  //predict mean and covariance 
 }
 
 void UKF::UpdateLidar(MeasurementPackage meas_package) {
